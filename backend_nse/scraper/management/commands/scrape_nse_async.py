c42cf22f6847451a183b7756b3315b0721da22ba
Yogesh-MG <yogesh.m.gowdru@gmail.com>
2025-09-08 15:51:32 +0530

    time loc changes to Asia

--- backend_nse/scraper/management/commands/scrape_nse_async.py
+++ backend_nse/scraper/management/commands/scrape_nse_async.py
@@ -1,19 +1,22 @@
 from django.core.management.base import BaseCommand
 from scraper.async_scraper import scrape_all_symbols
-import datetime
 import asyncio
 import time
+from datetime import datetime
+import pytz
+
+# Get current time in IST
 
 class Command(BaseCommand):
     help = 'Scrape NSE option chain asynchronously every minute during market hours'
 
     def handle(self, *args, **kwargs):
         print("Starting async NSE scraper...")
-        comand = True
         while True:
-            now = datetime.datetime.now()
+            ist = pytz.timezone('Asia/Kolkata')
+            now = datetime.now(ist)
             # Market hours: 9:14 → 15:10
-            if  comand:#(now.hour == 9 and now.minute >= 14) or (10 <= now.hour <= 14) or (now.hour == 15 and now.minute <= 10):
+            if  (now.hour == 9 and now.minute >= 14) or (10 <= now.hour <= 14) or (now.hour == 15 and now.minute <= 10):
                 asyncio.run(scrape_all_symbols())
             else:
                 print(f"[{now}] Market closed, waiting...")
